fb5f4e94eb97fe36a01d22f519fa3f412e9369c0
Jay Patel <jpatel8110@gmail.com>
2020-08-28 20:18:32 -0400

    All 3 Api calls implemented

--- src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -3,14 +3,14 @@ package io.javabrains.moviecatalogservice.resources;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,21 +21,29 @@ public class CatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        List<Rating> ratings= Arrays.asList(
-                new Rating("1234",4),
-                new Rating("5678",3)
-        );
+       UserRating ratings= restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId, UserRating.class);
 
-        return  ratings.stream().map(
+        return  ratings.getRatings().stream().map(
               rating -> {
-                  Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-
-                  return new CatalogItem(movie.getName(), "Description", rating.getRating());
+                  // for each movie id , call movie info service and get details
+               Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+               //Put them together
+               return new CatalogItem(movie.getName(), "Description", rating.getRating());
               }
         ).collect(Collectors.toList());
 
     }
 }
+  /* Movie movie =webClientBuilder.build() // For Sping FLUX
+                .get()
+                .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                      .retrieve()
+                      .bodyToMono(Movie.class)
+                      .block();*/
